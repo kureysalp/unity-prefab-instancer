01deab10358525935590eaa49af6be82980b8965
Kureyş Alp Kılıç <50348497+kureysalp@users.noreply.github.com>
2019-08-01 18:00:56 +0300

    Uploaded files.
    First upload.

---
+++ PrefabCorrector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PrefabCorrector : MonoBehaviour
+{
+    public List<GameObject> objectList = new List<GameObject>();
+    public GameObject prefab;
+    
+
+   public void CorrectPrefabs()
+    {
+        foreach (GameObject go in objectList)
+        {
+            Debug.Log("za");
+            Transform _transformData = go.transform;
+            Object _prefab = PrefabUtility.InstantiatePrefab(prefab as GameObject);
+            GameObject goPrefab = _prefab as GameObject;
+            goPrefab.transform.parent = go.transform.parent;
+            goPrefab.transform.position = _transformData.position;
+            goPrefab.transform.rotation = _transformData.rotation;
+            goPrefab.transform.localScale = _transformData.localScale;
+            DestroyImmediate(go);
+            _prefab.name = prefab.name;
+        }
+    }
+}
---
+++ PrefabCorrectorInspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PrefabCorrector))]
+public class PrefabCorrectorInspector : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PrefabCorrector _prefab = (PrefabCorrector)target;
+
+        if (GUILayout.Button("Correct"))
+        {
+            _prefab.CorrectPrefabs();
+        }
+    }
+
+
+
+}
